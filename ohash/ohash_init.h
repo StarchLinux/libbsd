175103eef8250d8f8a0c41fb9dff1982220f9b12
strake <strake888@gmail.com>
2012-08-07 15:05:40 -0500

    add ohash

---
+++ ohash/ohash_init.h
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<title>Error</title>
+<meta name="robots" content="nofollow" />
+<meta name="generator" content="FreeBSD-CVSweb 3.0.6" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<link rel="stylesheet" type="text/css" href="/cvsweb.css" />
+</head>
+<body>
+<img src="/images/smalltitle.gif"> <h1>Error</h1>
+<div id="error">Error: src/lib/libc/ohash/ohash_init.h: no such file or directory</div>
+<hr />
+<address><font size=-1>OpenBSD CVSweb server maintained by &lt;beck@openbsd.org&gt;</font></address>
+</body>
+</html>
